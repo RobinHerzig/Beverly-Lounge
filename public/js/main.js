303efba9e59e75318f905c93c5c2fa98e287d61f
Robin Herzig <robin.hrzg@gmail.com>
2022-06-20 00:26:55 -0500

    Updated homepage and main.js

--- public/js/main.js
+++ public/js/main.js
@@ -1,43 +1,24 @@
-console.log('Main.js is working')
-
-const deleteCard = document.querySelectorAll('.fa-solid fa-trash')
 const bookmarkCard = document.querySelectorAll('.fa-solid fa-bookmark')
-const unbookmarkCard = document.querySelectorAll('.fa-solid fa-bookmark-slash')
 
-Array.from(deleteCard).forEach((element)=>{
-    element.addEventListener('click', makeDeleteCard)
-})
+document.querySelector('#input').addEventListener('focusin', lightUpInput)
+document.querySelector('#input').addEventListener('focusout', turnOffInput)
+
 Array.from(bookmarkCard).forEach((element)=>{
     element.addEventListener('click', makeBookmarkCard)
 })
-Array.from(unbookmarkCard).forEach((element)=>{
-    element.addEventListener('click', makeUnbookmarkCard)
-})
 
-// async function makeDeleteCard(){
-//     const sName = this.parentNode.childNodes[1].innerText
-//     const bName = this.parentNode.childNodes[3].innerText
-//     try{
-//         const response = await fetch('deleteRapper', {
-//             method: 'delete',
-//             headers: {'Content-Type': 'application/json'},
-//             body: JSON.stringify({
-//               'stageNameS': sName,
-//               'birthNameS': bName
-//             })
-//           })
-//         const data = await response.json()
-//         console.log(data)
-//         location.reload()
+function lightUpInput() {
+    console.log('The input is focused.')
+    document.querySelector('#neonText').style.color = 'var(--neon)'
+    document.querySelector('#neonText').style.textShadow = '0 0 0.2em var(--white), 0 0 0.5em var(--neon)'
+}
 
-//     }catch(err){
-//         console.log(err)
-//     }
-// }
+function turnOffInput() {
+    console.log('The input is unfocused.')
+}
 
 function makeBookmarkCard() {
     console.log('Bookmark clicked')
-    this.classList.add('hidden')
     // Array.from(unbookmarkCard).element[0].classList.remove('hidden')
     // const id = info.drinks[i].idDrink
     // console.log(id)
